a5e15a9bbb731f07904e6e231e737f1c2125f3a1
ilmilda5549 <ilminda5549@gmail.com>
2022-06-14 21:54:25 +0700

    add new sourcecode and add .gitignore

---
+++ kor madi kub Md.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+	
+	int num ,i ;
+	for(i=i+1 ; i<=10 ; i++){
+	
+	
+		printf("Enter number%d:",i,num);
+		scanf("%d", &num);
+		if(num==20)
+		printf("correct\n");
+		else
+		printf("kor ma di kub\n");
+		}
+}
